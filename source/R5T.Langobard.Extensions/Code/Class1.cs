6b97bddde6cec78ed90e255675652f8a57f4d6a4
MinexAutomation <minex.automation@gmail.com>
2020-01-16 13:15:29 -0800

    Added the LoggingBuilderHelper class. Added the LoggingBuilderHelper.AddDefaultLogging() method.

--- source/R5T.Langobard.Extensions/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Langobard.Extensions
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Langobard.Extensions/Code/Classes/LoggingBuilderHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+
+namespace R5T.Langobard
+{
+    public class LoggingBuilderHelper
+    {
+        #region Static
+
+        /// <summary>
+        /// Adds console logging.
+        /// </summary>
+        private static void AddDefaultLogging(ILoggingBuilder loggingBuilder)
+        {
+            loggingBuilder.AddConsole();
+        }
+
+        #endregion
+    }
+}
