3a88af3b1ab59775122af1b80e1dec53d59db800
MinexAutomation <minex.automation@gmail.com>
2020-02-23 16:48:37 -0800

    Created and added ILoggingBuilder extensions.

---
+++ source/R5T.Langobard.Extensions/Code/Extensions/ILoggingBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+
+namespace R5T.Langobard
+{
+    public static class ILoggingBuilderExtensions
+    {
+        /// <summary>
+        /// Adds console logging.
+        /// </summary>
+        public static ILoggingBuilder AddBasicLogging(this ILoggingBuilder loggingBuilder)
+        {
+            loggingBuilder.AddConsole();
+
+            return loggingBuilder;
+        }
+    }
+}
