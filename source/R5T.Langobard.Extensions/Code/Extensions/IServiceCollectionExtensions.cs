cb0f0adabd12348c8a02b3685907629e7a44c12c
David Coats <david@getsafetycone.com>
2021-06-24 19:01:00 -0700

    Add AddDefaultLogging() method to now deprecated R5T.Langobard.Extensions repository.

---
+++ source/R5T.Langobard.Extensions/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDefaultLogging(this IServiceCollection services, LogLevel logLevel = LogLevel.Debug)
+        {
+            services
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder
+                        .AddConsole(consoleLoggerOptions =>
+                        {
+                            // Do nothing.
+                        })
+                        .SetMinimumLevel(logLevel);
+                        ;
+                })
+                ;
+
+            return services;
+        }
+    }
+}
